d4c018f8bc3bcda6348e52ae7fdec0e8964a009b
LG-Soria <96795781+LG-Soria@users.noreply.github.com>
2022-10-14 13:00:29 -0300

    Update app.js
    Borre comentarios que no iban.

--- app.js
+++ app.js
@@ -1,32 +1,4 @@
 'use strict'
-/*
-const grande = document.querySelector('.proyectosBox')
-const caja = document.querySelectorAll('.proyecto')
-const punto = document.querySelectorAll('.punto')
-
-    punto.forEach ( ( _cadaPunto, i ) => {
-    punto[i].addEventListener('click',()=>{
-        let posicion = i
-        //posicion es 0 transformX es 0
-        //posicion es 1 transformX es -50%
-        //operacion = posicion * -50
-
-        let operacion = posicion * -25
-
-        grande.style.transform = `translateX(${ operacion }%)`
-       
-
-
-        punto.forEach( (_cadaPunto, i ) => {
-            punto[i].classList.remove('activo')
-        })
-        punto[i].classList.add('activo')
-
-      
-   })
-})
-
-*/
 
 
 //CARRUSEL DE PROYECTOS
@@ -66,27 +38,3 @@ carrusel.addEventListener("mouseout", () => {
 
 start()
 
-
-
-
-const aboutDiv = document.getElementsByClassName('.aboutDiv');
-const c = aboutDiv.getelementsByTagName('*');
-const children = []
-
-for(var i = 0; i < c.length; i++) children.push(c[i]);
-
-function expandToFullHeight (evt) {
-    evt.target.style.height = evt.target.scrollHeight.toString() + "px";
-}
-
-function retract(evt){
-        if(children.indexOf(evt.relatedTarget) === -1) evt.target.style.height = "" ;
-
-    }
-
-    if(aboutDiv.addEventListener) {
-        aboutDiv.addEventListener("mouseover", expandToFullHeight, false);
-        aboutDiv.addEventListener("mouseout", retract           , false);
-            for(var i = 0; i < children.length; i++)
-            children[i].addEventListener("mouseout", function(evt){evt.stopPropagation();}, false)
-    }
